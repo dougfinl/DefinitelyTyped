5f1c162a11023b93e6fd2e52208c9834f0e44426
Douglas Finlay <douglas@douglasfinlay.com>
2020-10-11 13:39:17 +0100

    Add public API for osc-min

---
+++ types/osc-min/index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for osc-min 1.1
+// Project: https://github.com/russellmcc/node-osc-min
+// Definitions by: Douglas Finlay <https://github.com/dougfinl>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function applyAddressTransform(buffer: any, transform: any): any;
+
+export function applyMessageTransform(buffer: any, transform: any): any;
+
+export function dateToTimetag(date: any): any;
+
+export function fromBuffer(buffer: any, strict: any): any;
+
+export function timestampToTimetag(secs: any): any;
+
+export function timetagToDate(timetag: any): any;
+
+export function timetagToTimestamp(timetag: any): any;
+
+export function toBuffer(object: any, strict: any, opt: any): any;
